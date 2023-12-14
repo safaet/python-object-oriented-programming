9c22d6ff622ac7fa290eede213a43e576668cade
safaet <safaetjaman@gmail.com>
2023-12-14 21:02:30 +0600

    Challenge: Data classes

--- Start/Ch 4/challenge.py
+++ Start/Ch 4/challenge.py
@@ -5,16 +5,35 @@
 # The subclasses are required to override the magic method
 # that makes them sortable
 
+from dataclasses import dataclass
+from abc import ABC, abstractclassmethod
+
+@dataclass
 class Asset():
-    pass
+    price: float
+
+    @abstractclassmethod
+    def __lt__(self, other):
+       pass
     
-
+@dataclass
 class Stock(Asset):
-    pass
+    ticker: str
+    company: str
+
+    def __lt__(self, other):
+       return self.price < other.price
 
 
+@dataclass
 class Bond(Asset):
-    pass
+   description: str
+   duration: int
+   yieldamt: float
+
+   def __lt__(self, other):
+      return self.yieldamt < other.yieldamt
+    
 
 # ~~~~~~~~~ TEST CODE ~~~~~~~~~
 stocks = [
