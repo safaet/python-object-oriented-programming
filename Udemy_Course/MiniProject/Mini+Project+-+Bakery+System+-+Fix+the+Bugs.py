a90e2c5505c32a23e7fffbbb9e0cdd6c4f28f382
safaet <safaetjaman@gmail.com>
2024-03-25 05:31:37 +0600

    Udemy Assignment 3

---
+++ Udemy_Course/MiniProject/Mini+Project+-+Bakery+System+-+Fix+the+Bugs.py
@@ -0,0 +1,20 @@
+class Donut:
+		def _ini__(self, flavor, toppings, filling, size):
+				self.flavor = flavor
+				self.toppings = toppings
+				self.filling = filling
+				self.size = size
+
+class Customer:
+
+		def _init_(self, name, age, address, favorite_dessert):
+				self.name = name
+				self.age = age
+				self.address = address
+				self.favorite_dessert = favorite_dessert
+
+class Cake:
+		def __init__(self, flavor, price, quality):
+				self.flavor = flavor
+				self.price = price
+				self.quality = quality
