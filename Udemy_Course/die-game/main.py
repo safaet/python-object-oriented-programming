49229deb6d39c2edd57b8274faa7bbba9b56be28
safaet <safaetjaman@gmail.com>
2024-04-25 05:46:29 +0600

    Implementing the Die Class

---
+++ Udemy_Course/die-game/main.py
@@ -0,0 +1,16 @@
+import random
+
+class Die:
+
+  def __init__(self):
+    self._value = None
+
+  @property
+  def value(self):
+    return self._value
+  
+  def roll(self):
+    new_value = random.randint(1, 6)
+    self._value = new_value
+    return new_value
+  
