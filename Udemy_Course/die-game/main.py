83bce575ac73da575ae34693f3e8e73e256827d4
safaet <safaetjaman@gmail.com>
2024-04-25 06:10:40 +0600

    Implementing the Player class

--- Udemy_Course/die-game/main.py
+++ Udemy_Course/die-game/main.py
@@ -14,3 +14,34 @@ class Die:
     self._value = new_value
     return new_value
   
+
+
+class Player:
+    def __init__(self, die, is_computer=False):
+      self._die = die
+      self._is_computer=is_computer
+      self._counter = 10
+
+    @property
+    def die(self):
+        return self._die
+    
+    @property
+    def is_computer(self):
+        return self._is_computer
+    
+    @property
+    def counter(self):
+       return self._counter
+    
+    def increament_counter(self):
+        self._counter += 1
+
+    def decreament_counter(self):
+        self._counter -= 1
+
+    def roll_die(self):
+       return self._die.roll()
+    
+
+
