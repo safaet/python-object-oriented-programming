151d337bdc0efc1851af60244fe61657d773ec63
safaet <safaetjaman@gmail.com>
2024-05-06 06:31:22 +0600

    super() to refer to the Superclass

---
+++ Udemy_Course/Inheritance/super.py
@@ -0,0 +1,20 @@
+class Dog:
+ 
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+ 
+		
+class Poodle(Dog):
+ 
+    def __init__(self, name, age, code):
+        super().__init__(name, age)
+        self.code = code
+
+
+# This line:
+super().__init__(name, ager)
+
+# Is equivalent to the syntax that you learned:
+
+Dog.__init__(self, name, age)
