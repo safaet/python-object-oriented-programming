24007815b3a0d341ad67054b28c7b2cd57d27f75
safaet <safaetjaman@gmail.com>
2024-05-13 10:08:57 +0600

    Assignment 15 Complete

--- Udemy_Course/Assignment/Mini+Project+-+Flight.py
+++ Udemy_Course/Assignment/Mini+Project+-+Flight.py
@@ -1,6 +1,30 @@
 class Flight:
+    """Class that represents an international Flight.
+
+    Attributes:
+            number (str): the flight number represented as a string.
+            origin (str): a three-letter abbreviation of the country of origin. e.g. "USA".
+            destination (str): a three-letter abbreviation of the destination e.g. "USA".
+            num_passengers (int): an integer that represents the number of passengers that are currently registered for the flight.
+
+    Methods:
+            display_flight_data(): display the data of the flight in user-friendly format.
+    """
        
     def __init__(self, number, origin, destination, num_passengers, total_weight, pilot, crew):
+        """Initializes the values of the instance attributes of Flight.
+
+        Args:
+            number (str): the flight number represented as a string.
+            origin (str): a three-letter abbreviation of the country of origin. e.g. "USA".
+            destination (str): a three-letter abbreviation of the destination e.g. "USA".
+            num_passengers (int): an integer that represents the number of passengers that 
+                currently registered for the flight.
+            total_weight (float): the approximate weight of the flight including baggage, passengers,
+                fuel, crew, and other elements.
+            pilot(Pilot): the pilot assigned to the flight.
+            crew(list of Crew): the crew assigned to the flight.
+        """
         self.number = number
         self.origin = origin
         self.destination = destination
@@ -11,6 +35,7 @@ class Flight:
  
     @property
     def total_weight(self):
+        """Total Weight of the flight, including luggage, crew, and fuel."""
         return self._total_weight
  
     @total_weight.setter
@@ -20,6 +45,7 @@ class Flight:
  
     @property
     def pilot(self):
+        """Pilot assigned to the flight."""
         return self._pilot
  
     @pilot.setter
@@ -28,6 +54,7 @@ class Flight:
  
     @property
     def crew(self):
+        """Crew assigned to the flight."""
         return self._crew
 	
     @crew.setter
@@ -35,6 +62,7 @@ class Flight:
         self._crew = new_crew
  
     def display_flight_data(self):
+        """Print flight data in a user-friendly format."""
         print("== Flight ==")
         print("Number:", self.number)
         print("Number of Passengers:", self.num_passengers)
