1fe1cd7e476792f949b6138551759b25095f8f61
safaet <safaetjaman@gmail.com>
2024-05-16 18:29:44 +0600

    Insert a Node to the End of a Linked List

--- Udemy_Course/Projects/ImplementLinkedList/main.py
+++ Udemy_Course/Projects/ImplementLinkedList/main.py
@@ -5,10 +5,11 @@ my_linked_list = LinkedList()
 my_linked_list.insert_node(9)
 my_linked_list.insert_node(3)
 my_linked_list.insert_node(6)
-my_linked_list.insert_node(2)
+my_linked_list.insert_node(15)
 
 # 1st: 9
 # 2nd: 3 -> 9
 # 3rd: 3 -> 6 -> 9
+# 4th: 3 -> 6 -> 9 -> 15
 
 print(my_linked_list.head.next.next.next.value)
